efb87f9ff5458b9404785176f0880663669fa383
Sebastian Estrada <Sebastian34@berkeley.edu>
2023-12-19 13:10:48 -0800

    Added Tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<input class="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal" type="email" placeholder="jane@example.com">
+
+
+<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+    Button
+  </button>
+  <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+    Button
+  </button>
